0241baee363b48eb46e0210c88e607670c146358
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2024-01-11 22:33:37 +0200

    initial template for the FlappyBirdy game

---
+++ desktop/src/corp/siendev/com/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package corp.siendev.com;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import corp.siendev.com.FlappyBirdy;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("FlappyBirdy");
+		new Lwjgl3Application(new FlappyBirdy(), config);
+	}
+}
