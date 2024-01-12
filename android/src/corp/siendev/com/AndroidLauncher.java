e97b46975ce23c9207b1fb1b83dc8cfeaa3ca6a5
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2024-01-12 21:43:08 +0200

    AndroidLauncher

---
+++ android/src/corp/siendev/com/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package corp.siendev.com;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import corp.siendev.com.FlappyBirdy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBirdy(), config);
+	}
+}
